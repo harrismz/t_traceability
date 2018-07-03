988bee99f58fdd1b1fa23655f3aecc880646e547
JVCFDDOM001\eng70 <teguhsetiawan171@gmail.com>
2018-07-03 10:08:49 +0700

    add skeleton to front end

--- production/content/errorfinder/errorfinder.php
+++ production/content/errorfinder/errorfinder.php
@@ -42,17 +42,46 @@
 		</div>
 		<div class="col-md-6 col-sm-12 col-xs-12">
 			<div class="x_panel">
-				<div class="x_title"></div>
-				<div class="x_content"></div>
+				<div class="x_title">OLL File Upload</div>
+				<div class="x_content">
+					<div class="form-group">
+						<label for="file">Filename:</label>	
+						<input type="file" name="" class="form-control"  >
+					</div>
+					<div class="col-md-12" >
+						<div class="form-group">
+							<label for="Date">Date</label>	
+							<input placeholder="Date" type="input" name="" class="form-control"  >
+						</div>
+						<div class="form-group">
+							<label for="Date">Machine Name</label>	
+							<input type="input" name="" class="form-control"  >
+						</div>
+						<div class="form-group">
+							<label for="Date">Feeder Number</label>	
+							<input type="input" name="" class="form-control"  >
+						</div>
+						<div class="form-group">
+							<label for="Date">Part Number</label>	
+							<input type="input" name="" class="form-control"  >
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
-	</div>
-	
-	<div class="row">
-		<div class="col-md-12 col-sm-12 col-xs-12">
+		<div class="col-md-6 col-sm-12 col-xs-12">
 			<div class="x_panel">
-				<div class="x_title"></div>
-				<div class="x_content"></div>
+				<div class="x_title">Panacim Upload Form</div>
+				<div class="x_content">
+					<div class="form-group">
+						<label for="file">Panacim File:</label>	
+						<input type="file" name="" class="form-control "  >
+					</div>
+					<h2>Result</h2>
+					<div class="x_panel" >
+						<div class="x_content" id="panacim-list"></div>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
