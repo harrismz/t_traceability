e57c1ad31e0ad3fde1973d0c0e52178970a73404
JVCFDDOM001\eng70 <teguhsetiawan171@gmail.com>
2018-07-06 17:05:03 +0700

    crbController on progress

--- api/crb/CrbController.php
+++ api/crb/CrbController.php
@@ -5,26 +5,48 @@
  */
 class CrbController
 {
-	protected $parameter = [];
+	protected $file;
 
 	function __construct()
-	{
-		$allowedParameter = ['file'];
-
-		foreach ($_POST as $key => $value) {
-			// if (in_array($key, $allowedParameter)) {
-				# code...
-				$this->parameter[] = $key;
-			// }
+	{	
+		if (isset($_FILES['file'])) {
+			$this->file = $_FILES['file'];
 		}
 	}
 
 	public function upload(){
 		// get the file here.
-		// var_dump($this->parameter);
-		var_dump( $this->parameter[0]);
-		// return json_encode($this->parameter);
+		$file = $this->validate();
+		$content = file_get_contents($file);
+		// get table for part location. == 'PositionData<1>'
+		preg_match_all( '(\[(.*?)\])', $content , $matches);
+		
+		$new = [];
+		foreach ($matches[1] as $key => $match) {
+			$new[] = $match;
+		}
 
+		return json_encode($new);
+		// 
+	}
+
+	private function validate(){
+		// cek if error is there
+		if (!$this->file) {
+			throw new Exception("No File Found!", 1);
+		}
+
+		// cek error form $_FILES
+		if ($this->file['error'] != 0) {
+			throw new Exception("Error Processing Request", 1);
+		}
+
+		// cek if it is crb
+		if (substr( $this->file['name'] , -3 ) !== 'crb' )  {
+			throw new Exception("you need to upload .crb file ", 1);
+		}
+
+		return $this->file['tmp_name'];
 	}
 
 	public function download(){
