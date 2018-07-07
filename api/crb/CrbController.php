2239cf2fc6c9d0b0d29fb628fa509e1a86145752
JVCFDDOM001\eng70 <teguhsetiawan171@gmail.com>
2018-07-07 13:55:28 +0700

    getting value from crb file

--- api/crb/CrbController.php
+++ api/crb/CrbController.php
@@ -6,36 +6,100 @@
 class CrbController
 {
 	protected $file;
+	protected $parameters;
 
-	function __construct()
-	{	
+	public $allowedParameter = [
+		'part_location'
+	];
+
+	function __construct(){ 
+
 		if (isset($_FILES['file'])) {
 			$this->file = $_FILES['file'];
+		}
+
+		foreach ($_POST as $key => $value) {
+			if (in_array($key, $this->allowedParameter )) {
+				# code...
+				$this->parameters[$key] = $value;
+			}
 		}
 	}
 
 	public function upload(){
+
+		// return json_encode($this->parameters );
+
 		// get the file here.
 		$file = $this->validate();
-		$content = file_get_contents($file);
-		// get table for part location. == 'PositionData<1>'
-		preg_match_all( '(\[(.*?)\])', $content , $matches);
-		
-		$requiredTable = [
-			'PositionData<1>',
-			'PartsData',
-		];
+		$lines = file($file);
 
-		$new = [];
-		foreach ($matches[1] as $key => $match) {
-			if (in_array($match, $requiredTable)) {
-				# code...
-				$new[] = $match;
-			}
+		$positionData = $this->getTableContent('[PositionData<1>]'.PHP_EOL , $lines/*, ['IDNUM', 'C', 'PARTS']*/ ); //contain location name
+
+		$searchIndex = array_search($this->parameters['part_location'], array_column($positionData, 'C'));
+
+		$positionData = [$positionData[$searchIndex]];
+
+		$partsData = $this->getTableContent('[PartsData]'.PHP_EOL , $lines, ['IDNUM', 'NAME', 'C'] ); //contain parts number
+
+		return json_encode( $this->join($positionData, $partsData, 'PARTS', 'IDNUM') );
+	}
+
+	/*
+		function Array getTableContent ()
+		@param1 = name of table in file.crb such as [PositionData<1>]
+		@param2 = Array of content value; 
+		@param3 = array of column that need to be fetch
+	*/
+	private function getTableContent( $tableName, Array $lines, Array $include = [] ){
+		$index = array_search( $tableName , $lines);
+
+		if ($index == false ) {
+			return false;
 		}
 
-		return json_encode($new);
-		// 
+		$positionData = [];
+		$header = explode(' ', $lines[$index+1] );
+		// jika include tidak diisi,maka ambil semua kolom.
+		if ($include == [] ) {
+			$include = $header;
+		}
+		
+		$index += 2; //ditambah 2 untuk langsung ambil value nya saja. tidak dengan nama table & kolomnya.
+		// kalau diambil dari index, nama table & nama kolom kebawa.
+		for ($i=$index; $i < count($lines) ; $i++) { 
+			
+			if ($lines[$i] == PHP_EOL ) {
+				break; //stop looping if find enter char
+			}
+
+			// changes into associatif array 
+			$row = explode(' ', $lines[$i] );
+			$new_row=[];
+			foreach ($header as $key => $value) {
+				if (in_array($value, $include )) {
+					# preg_replace is for sanitizing the string. removing all special char
+					$new_row[$value] = preg_replace('/[^A-Za-z0-9\-]/', '', trim( $row[$key]) ); 
+				}
+			}
+
+			$positionData[] =  $new_row;
+		}
+		
+		return $positionData;
+	}
+
+	// similiar to join state ment 
+	private function join(Array $table1, Array $table2, $column1, $column2 ){
+		$result = [];
+		foreach ($table1 as $key => $valueTable1) {
+			foreach ($table2 as $key => $valueTable2 ) {
+				if ($valueTable1[$column1] == $valueTable2[$column2]) {
+					$result[] = array_merge_recursive($valueTable1, $valueTable2);
+				}
+			}
+		}
+		return $result;
 	}
 
 	private function validate(){
