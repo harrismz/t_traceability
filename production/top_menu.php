691652872b7b61e59b23592baccc866879edda17
JVCFDDOM001\edp5 <harrismz.git21@gmail.com>
2018-07-20 07:55:56 +0700

    Revise Top menu base on content of side menu.

--- production/top_menu.php
+++ production/top_menu.php
@@ -4,32 +4,7 @@
       <a id="menu_toggle"><i class="fas fa-bars"></i></a>
     </div>
     <ul class="nav navbar-nav navbar-right scrolltoid">
-      <li id="Probleminfo_menu" class="">
-        <a href="#" class="user-profile" aria-expanded="false">
-          Problem Information
-        </a>
-      </li>
-      <li id="finishgood_menu" class="">
-        <a href="#finishgood_tab" class="user-profile" aria-expanded="false">
-          Finished Good
-        </a>
-      </li>
-      <li id="oqc_menu" class="">
-        <a href="#oqc_tab" class="user-profile" aria-expanded="false">
-          OQC
-        </a>
-      </li>
-      <li id="process_menu" class="">
-        <a href="#process_tab" class="user-profile" aria-expanded="false">
-          Process Operational
-        </a>
-      </li>
-      <li id="parthandling_menu" class="">
-        <a href="#parthandling_tab" class="user-profile" aria-expanded="false">
-          Part Handling
-        </a>
-      </li>
-
+      <?php include 'menu/TopMenuSection.php';?>
     </ul>
   </nav>
 </div>
