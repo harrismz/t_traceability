3cac07f19e60eb7fd8f6493a842bd7354f477e7b
JVCFDDOM001\edp5 <harris.git21@gmail.com>
2018-09-18 18:41:36 +0700

    add measurement template

--- index.php
+++ index.php
@@ -159,6 +159,18 @@
 								include "production/page_404.html";
 							}
 				      	}
+				      	elseif($_REQUEST['content'] == "measurement"){
+							$page_dir11 = 'production/content/measurement';
+							$thispages11 = scandir($page_dir11);
+							unset($thispages11[0], $thispages11[11]);
+							$page11 = $_REQUEST['content'];
+							if(in_array($page11.'.php', $thispages11)){
+								include_once($page_dir11.'/'.$page11.'.php');
+							} else {
+								//echo 'Page not found! :( finishgood php';
+								include "production/page_404.html";
+							}
+				      	}
         		?>
 			</div><!-- /page content -->
 			<!-- footer content -->
---
+++ production/content/measurement/measurement.php
@@ -0,0 +1,102 @@
+<script type="text/javascript" src='./my/js/measurement/measurement.js'></script>
+<section>
+  <!--  Searching Traceability -->
+  <div class="row">
+    <div class="x_panel">
+      <div class="form-inline">
+        <div id="src_measurement_date" class="form-group col-xs-12 col-sm-12 col-md-12 col-lg-12">
+          <h5 class="x_title"><i class="fas fa-search"></i> Measurement date </h5>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--  END OF Searching Traceability -->
+  <!-- ============================================================================== -->
+  <!--  MAIN OCS Finishgood Model -->
+  <div class="row">
+    <div class="col-md-12 col-sm-12 col-xs-12">
+      <div class="x_panel">
+        <div class="x_title">
+  				<h2><i class="fab fa-hubspot"></i> THERMO </h2>
+  				<ul class="nav navbar-right panel_toolbox">
+  					<li>
+  						<a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+  					</li>
+  					<li>
+  						<a class="close-link"><i class="fas fa-times"></i></a>
+  					</li>
+  				</ul>
+  				<div class="clearfix"></div>
+  			</div>
+        <div class="x_content">
+          <div  id="panel_thermo" class="extjs_border">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--  END OF MAIN OCS Finishgood Model -->
+  <!-- ============================================================================== -->
+  <!-- Plan and Actual Data -->
+  <div class="row">
+    <div class="col-md-12 col-sm-12 col-xs-12">
+      <div class="x_panel">
+        <div class="x_title">
+  				<h2><i class="fa fa-shoe-prints"></i> ESD</h2>
+  				<ul class="nav navbar-right panel_toolbox">
+  					<li>
+  						<a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+  					</li>
+  					<li>
+  						<a class="close-link"><i class="fas fa-times"></i></a>
+  					</li>
+  				</ul>
+  				<div class="clearfix"></div>
+  			</div>
+        <div class="x_content">
+          <div id="panel_esd" class="extjs_border">
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-12 col-sm-12 col-xs-12">
+      <div class="x_panel">
+        <div class="x_title">
+  				<h2><i class="far fa-calendar-check"></i> TORQUE</h2>
+  				<ul class="nav navbar-right panel_toolbox">
+  					<li>
+  						<a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+  					</li>
+  					<li>
+  						<a class="close-link"><i class="fas fa-times"></i></a>
+  					</li>
+  				</ul>
+  				<div class="clearfix"></div>
+  			</div>
+        <div class="x_content">
+          <div  id="panel_torque" class="extjs_border">
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-12 col-sm-12 col-xs-12">
+      <div class="x_panel">
+        <div class="x_title">
+          <h2><i class="far fa-calendar-check"></i> TEMPERATURE</h2>
+          <ul class="nav navbar-right panel_toolbox">
+            <li>
+              <a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+            </li>
+            <li>
+              <a class="close-link"><i class="fas fa-times"></i></a>
+            </li>
+          </ul>
+          <div class="clearfix"></div>
+        </div>
+        <div class="x_content">
+          <div  id="panel_temperature" class="extjs_border">
+          </div>
+        </div>
+      </div>
+    </div>
+</section>
