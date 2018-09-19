f9be3b3a3abba70b963e9c508eaf236a0fb3c6d4
JVCFDDOM001\edp5 <harris.git21@gmail.com>
2018-09-19 09:09:32 +0700

    add view particle dusty data

--- production/content/measurement/measurement.php
+++ production/content/measurement/measurement.php
@@ -98,5 +98,25 @@
           </div>
         </div>
       </div>
+    </div> 
+    <div class="col-md-12 col-sm-12 col-xs-12">
+      <div class="x_panel">
+        <div class="x_title">
+          <h2><i class="far fa-calendar-check"></i> PARTICLE DUSTY</h2>
+          <ul class="nav navbar-right panel_toolbox">
+            <li>
+              <a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+            </li>
+            <li>
+              <a class="close-link"><i class="fas fa-times"></i></a>
+            </li>
+          </ul>
+          <div class="clearfix"></div>
+        </div>
+        <div class="x_content">
+          <div  id="panel_dusty" class="extjs_border">
+          </div>
+        </div>
+      </div>
     </div>
 </section>
