1a41fd8b2af8c7fd20b7f04d53344a908c2b8e5d
JVCFDDOM001\edp5 <harris.git21@gmail.com>
2018-09-19 14:09:21 +0700

    revise icon reflow with tachometer-alt

--- production/sidebar_menu.php
+++ production/sidebar_menu.php
@@ -34,7 +34,7 @@
         <li><a href="index.php?content=Production"><i class="fas fa-cogs"></i> Production </a></li> -->
         <li><a href="index.php?content=errorfinder"><i class="fas fa-exclamation-triangle"></i> Error Finder </a></li>
         <li><a href="index.php?content=measurement"><i class="fas fa-thermometer-half"></i> Measurement </a></li>
-        <li><a href="index.php?content=reflow"><i class="fas fa-memory"></i> Reflow </a></li>
+        <li><a href="index.php?content=reflow"><i class="fas fa-tachometer-alt"></i> Reflow </a></li>
 
 
 <!--
