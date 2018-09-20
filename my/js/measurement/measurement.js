cf337bf52d16bf8a745be4cd209c4781ed3128fa
JVCFDDOM001\edp5 <harris.git21@gmail.com>
2018-09-20 15:26:45 +0700

    revise NG status from --- to NG with red color

--- my/js/measurement/measurement.js
+++ my/js/measurement/measurement.js
@@ -5,11 +5,14 @@
 	//function untuk fontsize grid
 	function upsize(val) {
 		var x = val;
-		if (x == '' || x == 'NG' || x == '--'){
+		if (x == '' || x == '--'){
 			return '<font class="fontsize12" style="color:red;font-weight: bold;"> --- </font>';
 		}
 		else if (x == 'OK'){
 			return '<font class="fontsize12" style="color:green;font-weight: bold;"> ' + x + ' </font>';
+		}
+		else if (x == 'NG'){
+			return '<font class="fontsize12" style="color:red;font-weight: bold;"> ' + x + ' </font>';
 		}
 		else{
 			return '<font class="fontsize12">' + x + '</font>';
