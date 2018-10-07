8573f058e708b595e94666a078fa8ab5f26448e8
Harris Muhammad Zaki <harris.git21@gmail.com>
2018-10-07 08:20:29 +0700

    revise if finishgood SMT AOI Store is null.

--- json/finishgood_smt/json_good_smt_aoi_board.php
+++ json/finishgood_smt/json_good_smt_aoi_board.php
@@ -2,18 +2,27 @@
 	date_default_timezone_set('Asia/jakarta');
     include '../../../adodb/con_smtpros.php';
 
-    //$page 		= @$_REQUEST["page"];
-	//$limit 		= @$_REQUEST["limit"];
-	//$start		= (($page*$limit)-$limit)+1;
+    //$page 	= @$_REQUEST["page"];
+	//$limit 	= @$_REQUEST["limit"];
+	//$start	= (($page*$limit)-$limit)+1;
 	
-    $boardid      = $_REQUEST['boardid'];
-    $getdate    = substr($_REQUEST['smt_date'],0,10);
-	$smt_date 	= date('Y-m-d', strtotime($getdate));
+    $boardid    = @$_REQUEST['boardid'];
+    $smt_date2  = @$_REQUEST['smt_date'];
+    $getdate    = '';
+    $smt_date   = '';
 
+    if ($smt_date2) {
+        $getdate    = substr($_REQUEST['smt_date'],0,10);
+        $smt_date   = date('Y-m-d', strtotime($getdate));
+    }
+    else {
+        $smt_date   = '';
+    }
+    
 	//echo "exec traceability_smt_good_aoi_board '{$boardid}','{$smt_date}'";
-
     $rs         = $db->Execute("exec traceability_smt_good_aoi_board '{$boardid}','{$smt_date}'");
     $return     = array();
+
     for($i=0;!$rs->EOF;$i++){
 		$return[$i]['linkedserver']   = trim($rs->fields['0']);
         $return[$i]['pcbid']          = trim($rs->fields['1']);
@@ -26,10 +35,12 @@
        
         $rs->MoveNext();
     }
+
     $x = array(
-        "success"=>true,
-        //"totalCount"=>$totalcount,
-        "rows"=>$return);
+            "success"=>true,
+            "totalCount"=>$totalcount,
+            "rows"=>$return
+        );
 
     echo json_encode($x);
 
