edff52c75a2faa973b400602f6af2068f7fbd6b1
Harris Muhammad <harris.git21@gmail.com>
2018-11-18 08:20:08 +0700

    Add Critical Part MA in Finishgood

---
+++ json/finishgood_smt/json_good_smt_mapros_critical.php
@@ -0,0 +1,36 @@
+<?php
+	date_default_timezone_set('Asia/jakarta');
+    include '../../../adodb/con_mapros_MYSQL.php';
+
+
+    $model      = @$_REQUEST['model'];
+    $serial     = @$_REQUEST['serial_no'];
+    $boardid    = @$_REQUEST['boardid'];
+    $sql        = "call mapros_critical_test ('{$model}','{$serial}','{$boardid}')";
+    $rs         = $db->Execute($sql);
+    $return     = array();
+ 
+    for($i=0;!$rs->EOF;$i++){
+       
+        $return[$i]['unique_id']    = $rs->fields['0'];
+        $return[$i]['supp_code']    = $rs->fields['1'];
+        $return[$i]['part_no']      = $rs->fields['2'];
+        $return[$i]['po']           = $rs->fields['3'];
+        $return[$i]['prodsup']      = $rs->fields['4'];
+        $return[$i]['lotnosup']     = $rs->fields['5'];
+        $return[$i]['qty']          = $rs->fields['6'];
+        $return[$i]['scan_nik']     = $rs->fields['7'];
+        $return[$i]['created_at']   = $rs->fields['8'];
+        $return[$i]['process']      = $rs->fields['9'];
+        $return[$i]['code']         = $rs->fields['10'];
+        $return[$i]['line']         = $rs->fields['11'];
+        $rs->MoveNext();
+    }
+    $x = array(
+        "success"=>true,
+        //"totalCount"=>$totalcount,
+        "rows"=>$return);
+
+    echo json_encode($x);
+    $db->Close();
+?>
