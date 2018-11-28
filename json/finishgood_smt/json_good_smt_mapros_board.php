e57724317380790d4c471b53f94756def5af781d
Harris Muhammad <harris.git21@gmail.com>
2018-11-28 18:38:38 +0700

    (PCB SERIAL MAPROS BOARD) - Modification tampilan data record PCB dan menambahkan tactime proses.

--- json/finishgood_smt/json_good_smt_mapros_board.php
+++ json/finishgood_smt/json_good_smt_mapros_board.php
@@ -7,29 +7,31 @@
 	//$start		= (($page*$limit)-$limit)+1;
 	//  $getdate    = substr($_REQUEST['smt_date'],0,10);
     //  $smt_date   = date('Y-m-d', strtotime($getdate));
-
     $boardid    = @$_REQUEST['boardid'];
     $cavity     = @$_REQUEST['cavity'];
+    $model      = @$_REQUEST['model'];
+    $pwbname    = @$_REQUEST['pwbname'];
 
-
-	// echo "call traceability_goodsmt_board ('{$boardid}','{$cavity}')";
-    $rs    = $db->Execute("call traceability_goodsmt_board ('{$boardid}','{$cavity}')");
+	// echo "call traceability_goodsmt_board_rev2 ('{$boardid}','{$cavity}', '{$model}','{$pwbname}')";
+    $rs    = $db->Execute("call traceability_goodsmt_board_rev2 ('{$boardid}','{$cavity}', '{$model}','{$pwbname}')");
     $return = array();
  
     for($i=0;!$rs->EOF;$i++){
-        $return[$i]['board_id']      = trim($rs->fields['0']);
-        $return[$i]['guid_master']   = trim($rs->fields['1']);
-        $return[$i]['guid_ticket']   = trim($rs->fields['2']);
-        $return[$i]['modelname']     = trim($rs->fields['3']);
-        $return[$i]['lotno']         = trim($rs->fields['4']);
-        $return[$i]['scanner_id']    = trim($rs->fields['5']);
-        $return[$i]['status']        = trim($rs->fields['6']);
-        $return[$i]['scan_nik']      = trim($rs->fields['7']);
-        $return[$i]['judge']         = trim($rs->fields['8']);
-        $return[$i]['created_at']    = trim($rs->fields['9']);
-        $return[$i]['updated_at']    = trim($rs->fields['10']);
-        $return[$i]['lineprocess']   = trim($rs->fields['11']);
-        $return[$i]['line']          = trim($rs->fields['12']);
+        $return[$i]['tbone_id']             = trim($rs->fields['0']);
+        $return[$i]['tbone_line']           = trim($rs->fields['1']);
+        $return[$i]['tbone_model']          = trim($rs->fields['2']);
+        $return[$i]['tbone_boardid']        = trim($rs->fields['3']);
+        $return[$i]['tbone_lineprocess']    = trim($rs->fields['4']);
+        $return[$i]['IN_time']              = trim($rs->fields['5']);
+        $return[$i]['IN_judge']             = trim($rs->fields['6']);
+        $return[$i]['IN_nik']               = trim($rs->fields['7']);
+        $return[$i]['OUT_time']             = trim($rs->fields['8']);
+        $return[$i]['OUT_judge']            = trim($rs->fields['9']);
+        $return[$i]['OUT_nik']              = trim($rs->fields['10']);
+        $return[$i]['tbone_timerange']      = trim($rs->fields['11']);
+        $return[$i]['tbone_guidmaster']     = trim($rs->fields['12']);
+        $return[$i]['tbone_guidticket']     = trim($rs->fields['13']);
+        $return[$i]['tbone_idproces']       = trim($rs->fields['14']);
       
         $rs->MoveNext();
     }
