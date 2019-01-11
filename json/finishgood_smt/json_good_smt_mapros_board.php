c78733ba307b4faf6aa863a88dccc8d115a7355a
Harris Muhammad <harris.git21@gmail.com>
2019-01-11 09:45:34 +0700

    show data Mapros Board in svrdby ( Study )

--- json/finishgood_smt/json_good_smt_mapros_board.php
+++ json/finishgood_smt/json_good_smt_mapros_board.php
@@ -1,46 +1,93 @@
 <?php
-	date_default_timezone_set('Asia/jakarta');
-    include '../../../adodb/con_mapros_MYSQL.php';
+date_default_timezone_set('Asia/jakarta');
+include '../../../adodb/con_mapros_MYSQL.php';
 
-    //$page 		= @$_REQUEST["page"];
-	//$limit 		= @$_REQUEST["limit"];
-	//$start		= (($page*$limit)-$limit)+1;
-	//  $getdate    = substr($_REQUEST['smt_date'],0,10);
-    //  $smt_date   = date('Y-m-d', strtotime($getdate));
-    $boardid    = @$_REQUEST['boardid'];
-    $cavity     = @$_REQUEST['cavity'];
-    $model      = @$_REQUEST['model'];
-    $pwbname    = @$_REQUEST['pwbname'];
+//$page 		= @$_REQUEST["page"];
+//$limit 		= @$_REQUEST["limit"];
+//$start		= (($page*$limit)-$limit)+1;
+//  $getdate    = substr($_REQUEST['smt_date'],0,10);
+//  $smt_date   = date('Y-m-d', strtotime($getdate));
+$boardid    = @$_REQUEST['boardid'];
+$cavity     = @$_REQUEST['cavity'];
+$model      = @$_REQUEST['model'];
+$pwbname    = @$_REQUEST['pwbname'];
 
+/* check connection */
+if ($mysqli->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+}
+
+
+
+
+$sql    = "call traceability_goodsmt_board_rev2 ('{$boardid}','{$cavity}', '{$model}','{$pwbname}')";
+$rs     = $db->execute($sql);
+
+
+
+if ($result = $mysqli->query($query)) {
+
+    /* fetch associative array */
+    while ($row = $result->fetch_assoc()) {
+        printf ("%s (%s)\n", $row["Name"], $row["CountryCode"]);
+    }
+
+    /* free result set */
+    $result->free();
+}
+
+/* close connection */
+$mysqli->close();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 	// echo "call traceability_goodsmt_board_rev2 ('{$boardid}','{$cavity}', '{$model}','{$pwbname}')";
-    $rs    = $db->Execute("call traceability_goodsmt_board_rev2 ('{$boardid}','{$cavity}', '{$model}','{$pwbname}')");
-    $return = array();
+    $sql    = "call traceability_goodsmt_board_rev2 ('{$boardid}','{$cavity}', '{$model}','{$pwbname}')";
+    $rs     = $db->execute($sql);
+    //$return = array();
  
-    for($i=0;!$rs->EOF;$i++){
-        $return[$i]['tbone_id']             = trim($rs->fields['0']);
-        $return[$i]['tbone_line']           = trim($rs->fields['1']);
-        $return[$i]['tbone_model']          = trim($rs->fields['2']);
-        $return[$i]['tbone_boardid']        = trim($rs->fields['3']);
-        $return[$i]['tbone_lineprocess']    = trim($rs->fields['4']);
-        $return[$i]['IN_time']              = trim($rs->fields['5']);
-        $return[$i]['IN_judge']             = trim($rs->fields['6']);
-        $return[$i]['IN_nik']               = trim($rs->fields['7']);
-        $return[$i]['OUT_time']             = trim($rs->fields['8']);
-        $return[$i]['OUT_judge']            = trim($rs->fields['9']);
-        $return[$i]['OUT_nik']              = trim($rs->fields['10']);
-        $return[$i]['tbone_timerange']      = trim($rs->fields['11']);
-        $return[$i]['tbone_guidmaster']     = trim($rs->fields['12']);
-        $return[$i]['tbone_guidticket']     = trim($rs->fields['13']);
-        $return[$i]['tbone_idproces']       = trim($rs->fields['14']);
+    while(!$rs->EOF){
+
+        echo $rs->fields['4'].'<br>';
+
+        // $return[$i]['tbone_id']             = trim($rs->fields['0']);
+        // $return[$i]['tbone_line']           = trim($rs->fields['1']);
+        // $return[$i]['tbone_model']          = trim($rs->fields['2']);
+        // $return[$i]['tbone_boardid']        = trim($rs->fields['3']);
+        // $return[$i]['tbone_lineprocess']    = trim($rs->fields['4']);
+        // $return[$i]['IN_time']              = trim($rs->fields['5']);
+        // $return[$i]['IN_judge']             = trim($rs->fields['6']);
+        // $return[$i]['IN_nik']               = trim($rs->fields['7']);
+        // $return[$i]['OUT_time']             = trim($rs->fields['8']);
+        // $return[$i]['OUT_judge']            = trim($rs->fields['9']);
+        // $return[$i]['OUT_nik']              = trim($rs->fields['10']);
+        // $return[$i]['tbone_timerange']      = trim($rs->fields['11']);
+        // $return[$i]['tbone_guidmaster']     = trim($rs->fields['12']);
+        // $return[$i]['tbone_guidticket']     = trim($rs->fields['13']);
+        // $return[$i]['tbone_idproces']       = trim($rs->fields['14']);
       
         $rs->MoveNext();
     }
+
     $x = array(
         "success"=>true,
         //"totalCount"=>$totalcount,
         "rows"=>$return);
 
-    echo json_encode($x);
-    $rs->Close();
-    $db->Close();
+    echo json_encode($x);*/
+    $rs->close();
+    $db->close();
 ?>
