e54d4922d5f7a69f45e4fd73e89526f703e14393
Harris Muhammad <harris.git21@gmail.com>
2019-04-12 17:16:53 +0700

    Add New Menu Subsystem list and Working Instruction List (2)

---
+++ production/content/help/help.php
@@ -0,0 +1,35 @@
+<section>
+  <!--  Searching Traceability -->
+  <div class="page-title">
+    <div class="title_left">
+      <h3>MANUAL INSTRUCTION</h3>
+    </div>
+    <div class="clearfix"></div>
+    <div class="row">
+      <div class="col-md-12 col-sm-12 col-xs-12">
+        <div class="x_panel">
+          <div class="x_title">
+            <h2><i class="fas fa-code-branch"></i> Traceability</h2>
+            <ul class="nav navbar-right panel_toolbox">
+              <li>
+                <a class="collapse-link">
+                  <i class="fa fa-chevron-up"></i>
+                </a>
+              </li>
+            </ul>
+            <div class="clearfix"></div>
+          </div>
+          <div class="x_content">
+            <!-- <p>Add the class <code>.btn .btn-app</code> tag</p> -->
+            <a class="btn btn-app" href="my/Asset/ManualBook/Traceability.pptx" target=_blank>
+              <i class="fa fa-code-branch"></i> Traceability Instruction
+            </a>
+          </div>
+        </div>
+      </div>
+    </div> 
+  </div>
+  <!-- ============================================================================== -->
+  <!-- Return to Top -->
+  <a href="javascript:" id="return-to-top"><i class="fas fa-chevron-up"></i></a>
+</section>
---
+++ production/content/subsystem/subsystem.php
@@ -0,0 +1,132 @@
+<section>
+  <!--  Searching Traceability -->
+  <div class="page-title">
+    <div class="title_left">
+      <h3>SUB SYSTEM</h3>
+    </div>
+    <div class="clearfix"></div>
+    <div class="row">
+      <div class="col-md-12 col-sm-12 col-xs-12">
+        <div class="x_panel">
+          <div class="x_title">
+            <h2><i class="fas fa-code-branch"></i> SMT <small>Sub System</small></h2>
+            <ul class="nav navbar-right panel_toolbox">
+              <li>
+                <a class="collapse-link">
+                  <i class="fa fa-chevron-up"></i>
+                </a>
+              </li>
+            </ul>
+            <div class="clearfix"></div>
+          </div>
+          <div class="x_content">
+            <!-- <p>Add the class <code>.btn .btn-app</code> tag</p> -->
+            <a class="btn btn-app" href="http://136.198.117.48/pcbserial/index.php?content=pcbserial" target=_blank>
+              <i class="fa fa-code-branch"></i> SMT PCB Serial
+            </a>
+            <a class="btn btn-app" href="http://136.198.117.48/metalmask" target=_blank>
+              <i class="fa fa-theater-masks"></i> Metal Mask
+            </a>
+            <a class="btn btn-app" href="http://136.198.117.48/feedercheck" target=_blank>
+              <i class="fa fa-ruler-horizontal"></i> Feeder
+            </a>
+            <a class="btn btn-app" href="http://136.198.117.48/ledrank" target=_blank>
+              <!-- <span class="badge bg-red">6</span> -->
+              <i class="fa fa-lightbulb"></i> LED Rank
+            </a>
+            <a class="btn btn-app" href="http://136.198.117.48/criticalpart_oem" target=_blank>
+              <i class="fa fa-exclamation-triangle"></i> Critical Part
+            </a>
+            <a class="btn btn-app" href="http://136.198.117.48/imzdbs" target=_blank>
+              <!-- <span class="badge bg-green">211</span> -->
+              <i class="fa fa-edit"></i> IMZDBS
+            </a>
+            <a class="btn btn-app" href="http://136.198.117.48/big_system24" target=_blank>
+              <!-- <span class="badge bg-orange">32</span> -->
+              <i class="fa fa-qrcode"></i> BIGS
+            </a>
+            <a class="btn btn-app" href="http://136.198.117.48/mounter" target=_blank>
+              <!-- <span class="badge bg-orange">12</span> -->
+              <i class="fa fa-microchip"></i> Mounter
+            </a>
+            <a class="btn btn-app" href="http://136.198.117.48/printmaker" target=_blank>
+              <!-- <span class="badge bg-blue">102</span> -->
+              <i class="fa fa-print"></i>Print Maker
+            </a>
+          </div>
+        </div>
+      </div>
+    </div> 
+  </div>
+  <div class="clearfix"></div>
+    <div class="row">
+      <div class="col-md-12 col-sm-12 col-xs-12">
+        <div class="x_panel">
+          <div class="x_title">
+            <h2><i class="fas fa-code-branch"></i> MA <small>Sub System</small></h2>
+            <ul class="nav navbar-right panel_toolbox">
+              <li>
+                <a class="collapse-link">
+                  <i class="fa fa-chevron-up"></i>
+                </a>
+              </li>
+            </ul>
+            <div class="clearfix"></div>
+          </div>
+          <div class="x_content">
+            <!-- <p>Add the class <code>.btn .btn-app</code> tag</p> -->
+            <a class="btn btn-app" href="http://136.198.117.48/mapros_system54/public" target=_blank>
+              <i class="fa fa-cogs"></i> OEM Mapros
+            </a>
+            <a class="btn btn-app" href="http://136.198.117.48/maprosdownload" target=_blank>
+              <i class="fa fa-file-download"></i> Mapros Download
+            </a>
+            <a class="btn btn-app" href="http://136.198.117.48/production-schedule/preview" target=_blank>
+              <!-- <span class="badge bg-orange">32</span> -->
+              <i class="fa fa-calendar-alt"></i> Production Schedule
+            </a>
+            <a class="btn btn-app" href="http://136.198.117.48/trace_problem" target=_blank>
+              <!-- <span class="badge bg-orange">12</span> -->
+              <i class="fa fa-exclamation-circle"></i> Problem Information
+            </a>
+            <a class="btn btn-app" href="http://136.198.117.48/mapros/index.php?content=rework" target=_blank>
+              <!-- <span class="badge bg-orange">12</span> -->
+              <i class="fa fa-retweet"></i> Register Rework OEM
+            </a>
+            <a class="btn btn-app" href="http://136.198.117.78/mapros_system54/public/login/" target=_blank>
+              <!-- <span class="badge bg-orange">12</span> -->
+              <i class="fa fa-clipboard-list"></i> Data Transaction Mapros
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="clearfix"></div>
+    <div class="row">
+      <div class="col-md-12 col-sm-12 col-xs-12">
+        <div class="x_panel">
+          <div class="x_title">
+            <h2><i class="fas fa-code-branch"></i> LOGISTIC <small>Sub System</small></h2>
+            <ul class="nav navbar-right panel_toolbox">
+              <li>
+                <a class="collapse-link">
+                  <i class="fa fa-chevron-up"></i>
+                </a>
+              </li>
+            </ul>
+            <div class="clearfix"></div>
+          </div>
+          <div class="x_content">
+            <!-- <p>Add the class <code>.btn .btn-app</code> tag</p> -->
+            <a class="btn btn-app" href="http://136.198.117.48/logistic" target=_blank>
+              <i class="fa fa-pallet"></i> LOGISTIC
+            </a>
+          </div>
+        </div>
+      </div>
+    </div> 
+  </div>
+  <!-- ============================================================================== -->
+  <!-- Return to Top -->
+  <a href="javascript:" id="return-to-top"><i class="fas fa-chevron-up"></i></a>
+</section>
